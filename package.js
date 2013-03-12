b5974339064af67622d752a3600d0876ad4e8d16
Sawyer Bateman <sawyerbateman@gmail.com>
2013-03-11 23:45:28 -0600

    Trying to fix handlebars registerHelper not firing.

--- package.js
+++ package.js
@@ -4,20 +4,16 @@ Package.describe({
   summary: "A markdown parser and compiler. Built for speed."
 });
 
-// XXX hack -- need a way to use a package at bundle time
-var _ = require('../../packages/underscore/underscore.js');
-
 Package.on_use(function (api, where) {
   where = where || ["client", "server"];
   where = where instanceof Array ? where : [where];
 
   api.add_files("marked/lib/marked.js", where);
 
-  // XXX what we really want to do is, load template-integration after
-  // handlebars, iff handlebars was included in the project.
-  if (where === "client" ||
-      (where instanceof Array && _.indexOf(where, "client") !== -1)) {
-    api.use("handlebars", "client");
-    api.add_files("template-integration.js", "client");
+  for(var i = 0; i < where.length; i++) {                                       
+    if(where[i] === "client") {
+        api.use("handlebars", "client");
+        api.add_files("template-integration.js", "client");
+    }
   }
 });
